07056037de7812efdff6e80d097a32fe74e7a702
Daisy Tonui <tonuidaisy@gmail.com>
2023-09-11 15:29:53 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,15 +14,15 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{
-		printf("d% is positiv \n", n);
+		printf("%d is positiv \n", n);
 	}
 	else if (n < 0)
 	{
-		printf("d% is negative\n", n);
+		printf("%d is negative\n", n);
 	}
 	else
 	{
-		printf("d% is zero\n", n);
+		printf("%d is zero\n", n);
 	}
 	return (0);
 }
