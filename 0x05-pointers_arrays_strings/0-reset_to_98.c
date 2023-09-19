c045dba0ae7bb70af743cf111f2e64ad731b7741
Daisy Tonui <tonuidaisy@gmail.com>
2023-09-19 16:35:10 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,4 +1,8 @@
 #include "main.h"
+/**
+ * reset_to_98 - is the name of the function
+ * @n: is the function parameter
+ */
 
 void reset_to_98(int *n)
 
---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+
+void swap_int(int *a, int *b)
+
+{
+	int *a;
+	int *b;
+
+	*a = 42;
+	*b = 98;
+
+}	
