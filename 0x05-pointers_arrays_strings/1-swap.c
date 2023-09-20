83d2436ba43af8e78bff8d62ae58263317e78d7e
Daisy Tonui <tonuidaisy@gmail.com>
2023-09-19 17:23:48 -0700

    1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,13 +1,15 @@
 #include "main.h"
 
+/**
+ * swap_int - is the name of the function
+ * @a: is the name of the first parameter declared
+ * @b: is the name of the second parameter
+ */
 
 void swap_int(int *a, int *b)
-
 {
-	int *a;
-	int *b;
-
-	*a = 42;
-	*b = 98;
-
-}	
+int d;
+d = *a;
+*a = *b;
+*b = d;
+}
