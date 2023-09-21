71633f87c61ce252051478dd1b65d8d669641bf1
Daisy Tonui <tonuidaisy@gmail.com>
2023-09-21 15:26:50 -0700

    3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strcmp - is the name of the function
+ * @s1: is the first parameter
+ * @s2: is the second parameter
+ *
+ * Return: s1[a] - s2[a]
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int a;
+
+	a = 0;
+	while (s1[a] != '\0' && s2[a] != '\0')
+	{
+		if (s1[a] != s2[a])
+		{
+			return (s1[a] - s2[a]);
+		}
+		a++;
+	}
+	return (0);
+}
+
