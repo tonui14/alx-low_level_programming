250ddbde3fb517692b59e23c28218d359fa42609
Daisy Tonui <tonuidaisy@gmail.com>
2023-09-21 14:35:50 -0700

    1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * _strncat - is the name of the function
+ * @dest: is the first parameter
+ * @src: is the second parameter
+ * @n: is the third parameter
+ *
+ * Return: dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int a;
+	int j;
+
+	a = 0;
+	while (dest[a] != '\0')
+	{
+		a++;
+	}
+	b = 0;
+	while (b < n && src[b] != '\0')
+	{
+	dest[b] = src[b];
+	a++;
+	b++;
+	}
+	dest[a] = '\0';
+	return (dest);
+}
+
