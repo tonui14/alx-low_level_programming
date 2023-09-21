3c0813f03832e19bbc0801a057f7582900ff59fa
Daisy Tonui <tonuidaisy@gmail.com>
2023-09-21 16:01:08 -0700

    100-rot13.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * rot13 - is the encoder rot13
+ * @s: is the pointer to string params
+ *
+ * Return: *s
+ */
+
+char *rot13(char *s)
+{
+	int a;
+	int b;
+	char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (a = 0; s[a] != '\0'; a++)
+	{
+		for (b = 0; b < 52; b++)
+		{
+			if (s[a] == data1[b])
+			{
+				s[a] = datarot[b];
+				break;
+			}
+		}
+	}
+	return (s);
+}
+
