33e25b7f6cacdd25229f18220a8b915c2b6c0ecc
Daisy Tonui <tonuidaisy@gmail.com>
2023-10-05 11:00:41 -0700

    Update

---
+++ 0x09-static_libraries/main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - Entry function of the program
+ * This function is the entry point of the program that prints a quote by Kevin
+ * and returns 0 to indicate successful execution
+ *
+ * Return: Always 0 (Success)
+ *
+ **/
+
+int main(void)
+{
+	_puts("\"At the end of the day, my goal was to be the best hacker\"\n"
+		"\t- Kevin Mitnick");
+	return (0);
+}
