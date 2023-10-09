161693de208fb38622f710120661982cbccd10c2
Daisy Tonui <tonuidaisy@gmail.com>
2023-10-09 16:09:33 -0700

    2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main- This function prints the name of the compiled file.
+ * Return: (0) on successful execution
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
